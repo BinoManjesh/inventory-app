bcb151228801a030be4ae39d4f656a23528fe17e
BinoManjesh <binomanjesh@gmail.com>
2024-04-18 13:18:50 +0530

    Add all items page

--- controllers/item.js
+++ controllers/item.js
@@ -1,7 +1,12 @@
+import { Item } from "../models/index.js";
+
 export async function create_get() {}
 export async function create_post() {}
 
-export async function list() {}
+export async function list(req, res) {
+  const items = await Item.find({}).sort({ category: 1 }).exec();
+  res.render("item_list", { items });
+}
 
 export async function detail() {}
 export async function update_get() {}

8bd3d9b9a60996ceecd4df5a175531a69c89e8f1
BinoManjesh <binomanjesh@gmail.com>
2024-04-23 17:39:53 +0530

    Add create item page

--- controllers/item.js
+++ controllers/item.js
@@ -1,8 +1,57 @@
-import { Item } from "../models/index.js";
+import { Item, Category } from "../models/index.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { body, validationResult } from "express-validator";
+import { toHeadingCase } from "../utils/sanitizers.js";
 
-export async function create_get() {}
-export async function create_post() {}
+export const create_get = asyncHandler(async (req, res) => {
+  const categories = await Category.find().exec();
+  res.render("item_create", { categories });
+});
+
+export const create_post = [
+  body("name", "Name field is required")
+    .trim()
+    .notEmpty()
+    .bail()
+    .customSanitizer(toHeadingCase)
+    .custom(async (value) => {
+      const exists = await Item.exists({ name: value }).exec();
+      if (exists) {
+        throw new Error("Item already exists");
+      }
+    }),
+  body("description").optional().trim(),
+  body("category", "Category is required")
+    .notEmpty()
+    .custom(async (value) => {
+      const exists = await Category.exists({ _id: value }).exec();
+      if (!exists) {
+        throw new Error("Category does not exist");
+      }
+    }),
+  body("price", "Price must be a positive number").isInt({ min: 0 }),
+  body("number_in_stock", "Number in stock must be a positive number").isInt({
+    min: 0,
+  }),
+
+  asyncHandler(async (req, res) => {
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+    });
+    const results = validationResult(req);
+    if (!results.isEmpty()) {
+      const categories = await Category.find().exec();
+      res.render("item_create", { categories, item, errors: results.array() });
+      return;
+    }
+    await item.save();
+    res.redirect(item.url);
+  }),
+];
 
 export const list = asyncHandler(async function (req, res) {
   const items = await Item.find({}).sort({ category: 1 }).exec();
